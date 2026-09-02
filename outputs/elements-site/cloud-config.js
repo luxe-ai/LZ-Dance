b44311af465e8c33f18f0ceb9fa86a9750da7b9b
Lysss <xuheluo.xhl@gmail.com>
2026-09-02 14:56:23 +0800

    Restore outputs/elements-site/cloud-config.js

---
+++ outputs/elements-site/cloud-config.js
@@ -0,0 +1,6 @@
+globalThis.HIPHOP_CLOUD_CONFIG = Object.freeze({
+  supabaseUrl: "https://bnjsiltlknqmsnplyjyq.supabase.co",
+  supabasePublishableKey: "sb_publishable_peizXMU_IqV7-7nQeKl35A_BFht38tJ",
+  r2WorkerUrl: "https://bnjsiltlknqmsnplyjyq.supabase.co/functions/v1/practice-video-vault"
+});
+
